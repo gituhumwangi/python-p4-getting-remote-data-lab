f6c9c2808b8f03ce28ad7a210980c6a8d19af517
dennis <dennis.mwangi@student.moringaschool.com>
2023-09-25 00:09:03 +0300

    Completed lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -8,6 +8,21 @@ class GetRequester:
 
     def get_response_body(self):
         pass
+        URL = " https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+        response = requests.get(URL)
+        return response.content
 
     def load_json(self):
         pass
+        people_lists = []
+        people = json.load(self.get_response_body())
+        for person in people:
+            people_lists.append(person["name"])
+        return people_lists
+    
+people = GetRequester()
+
+people_names = people.load_json()
+
+for name in set(people_names):
+    print(name)
